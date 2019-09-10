e8f4fdccc5911bfcb90f34a713ea59cb7318d2bb
Atri Das <babinsamrat123@gmail.com>
2019-09-10 20:56:51 +0530

    adds String methods

--- bin/BootcampDay1/strings.dart
+++ bin/BootcampDay1/strings.dart
@@ -7,4 +7,13 @@ main(){
   print(string.codeUnits);
   print(string.isEmpty);
   print(string.length);
+
+  //String Methods
+
+  print(string.toLowerCase());
+  print(string.trim());
+  print(string.compareTo("Hi Dart Learners"));
+  print(string.replaceAll("Hello","Hey"));
+  print(string.substring(2));
+  print(string.split("Hello"));
 }
