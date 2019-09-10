755fceac64944deb48f552d8b84353660a95d53f
Atri Das <babinsamrat123@gmail.com>
2019-09-10 20:36:03 +0530

    adds number properties

---
+++ bin/BootcampDay1/numbers.dart
@@ -0,0 +1,11 @@
+main(){
+
+  int num = 10;
+  // Number Properties
+  print(num.hashCode);
+  print(num.isFinite);
+  print(num.isNaN);
+  print(num.isNegative);
+  print(num.sign);
+  print(num.isOdd);
+}
