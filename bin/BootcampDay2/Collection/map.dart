ea62770a6c15417b7d8c8f44e3c6b86a18970387
Atri Das <babinsamrat123@gmail.com>
2019-09-10 22:12:23 +0530

    adds map. but need to fix

---
+++ bin/BootcampDay2/Collection/map.dart
@@ -0,0 +1,13 @@
+main(){
+
+  var ranking = Map();
+  for(var i=0;i<3;i++){
+    for(var j=0;j<3;j++){
+      ranking.addAll({i:j});
+    }
+  }
+
+  for(var i=0;i<ranking.length;i++){
+    print(ranking[i]);
+  }
+}
