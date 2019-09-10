689da9964accb9b1944a56e997c48690e801dcc2
Atri Das <babinsamrat123@gmail.com>
2019-09-10 20:40:31 +0530

    adds Number Methods

--- bin/BootcampDay1/numbers.dart
+++ bin/BootcampDay1/numbers.dart
@@ -1,11 +1,21 @@
 main(){
 
-  int num = 10;
+  var num = 10.35;
+
   // Number Properties
   print(num.hashCode);
   print(num.isFinite);
   print(num.isNaN);
   print(num.isNegative);
   print(num.sign);
-  print(num.isOdd);
+  //print(num.isOdd);
+
+  //NUmber Methods
+  print(num.abs());
+  print(num.ceil());
+  print(num.compareTo(12));
+  print(num.floor());
+  print(num.remainder(3));
+  print(num.round());
+  print(num.truncate());
 }
